a9699515bc8236612ed88fada76fbf382a357281
Luis Barrichello <luisgbarrichelo@gmail.com>
2025-08-26 10:02:00 -0300

    feat(api): Add healthcheck endpoint
    Creates a simple, unauthenticated healthcheck endpoint at /api/v1/healthcheck/.
    
    This endpoint can be used by monitoring services to verify that the application is operational.
    
    Closes #10

--- setup/api_urls.py
+++ setup/api_urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from .views import HealthCheckView
 
 urlpatterns = [
+    # Healthcheck endpoint
+    path('healthcheck/', HealthCheckView.as_view(), name='healthcheck'),
+
     # Authentication endpoints
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
