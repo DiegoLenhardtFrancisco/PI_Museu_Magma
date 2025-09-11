d48fa28b215fec65c9dbfa0eb1f46b3a0a1475d3
Luís Gabriel Barrichello <luisgbarrichelo@gmail.com>
2025-09-10 22:09:33 -0300

    fix(config): Corrects the selection of settings for the production environment The wsgi.py file was set to use the development settings, causing ALLOWED_HOSTS to be empty in production. This fix introduces logic to use ‘setup.production’ when in the Render environment, resolving the DisallowedHost error.

--- setup/production.py
+++ setup/production.py
@@ -1,11 +1,1 @@
-import os
-
 from .base import *  # noqa
-
-DEBUG = False
-
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
-if not SECRET_KEY:
-    raise ValueError("A variável de ambiente DJANGO_SECRET_KEY não foi definida.")
-
-ALLOWED_HOSTS = ['seu-dominio.com', 'www.seu-dominio.com']
