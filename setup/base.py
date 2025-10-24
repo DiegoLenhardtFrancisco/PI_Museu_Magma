0fc5c84e691047d858fa93abcebe2180f9ac0e65
Luis Barrichello <luisgbarrichelo@gmail.com>
2025-10-24 08:14:02 -0300

    feat: Allow frontend URLs in CORS configuration

--- setup/base.py
+++ setup/base.py
@@ -191,6 +191,7 @@ SPECTACULAR_SETTINGS = {
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",  # Endereço React em desenvolvimento
     "https://museu-magma-frontend.vercel.app",
+    "https://sistemamuseumagma.vercel.app",
 ]
 
 CORS_ALLOW_CREDENTIALS = True
