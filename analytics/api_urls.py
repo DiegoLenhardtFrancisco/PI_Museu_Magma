585e8e4886a343767c6f19c149d59b5ce245e123
DiegoLenhardtFrancisco <diego.lenhardt@gmail.com>
2026-05-02 22:00:10 -0300

    style(analytics): fix isort formatting (api_urls)

--- analytics/api_urls.py
+++ analytics/api_urls.py
@@ -1,17 +1,12 @@
-"""
-analytics/api_urls.py
-
-URL configuration for the analytics app.
-"""
-
 from django.urls import path
 
 from .api_views import DashboardView
 
+
 urlpatterns = [
     path(
-        'analytics/dashboard/',
+        "analytics/dashboard/",
         DashboardView.as_view(),
-        name='analytics-dashboard'
+        name="analytics-dashboard",
     ),
 ]
